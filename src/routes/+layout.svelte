bf3ff962fb8407ef479016b5e9739fb5689d142a
Andrey <190183925+ankddev@users.noreply.github.com>
2025-10-21 18:40:50 +0300

    feat: add basic Vim navigation
    Add support of j and k navigation to website

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -21,11 +21,29 @@
 		keywords: 'blog, ankddev, dev, tech, programming, it, rust, golang, gleam, go',
 		description: '/dev/ankddev - personal site, blog and portfolio of ankddev'
 	};
+
+	function viewportHeight() {
+		return window.innerHeight;
+	}
+
+	function handleKeyDown(event: KeyboardEvent) {
+		const { key } = event;
+		switch (key) {
+			case 'j':
+				window.scrollBy(0, 0.5 * viewportHeight());
+				break;
+			case 'k':
+				window.scrollBy(0, -0.5 * viewportHeight());
+				break;
+		}
+	}
 </script>
 
 <svelte:head>
 	<title>{meta.title}</title>
 	<meta name="keywords" content={meta.keywords} />
 </svelte:head>
+
+<svelte:window onkeydown={handleKeyDown} />
 
 {@render children()}
