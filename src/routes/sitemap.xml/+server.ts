b650b1c541d28dce0978e28363abcfc2f0496855
Andrey <190183925+ankddev@users.noreply.github.com>
2025-10-24 06:33:11 +0300

    feat(seo): add sitemap.xml

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,19 @@
+import type { ArticlesResponse } from '@/lib/types';
+import type { RequestHandler } from '@sveltejs/kit';
+import * as sitemap from 'super-sitemap';
+
+export const GET: RequestHandler = async (event) => {
+	const response = await event.fetch('/api/articles');
+	const data: ArticlesResponse = await response.json();
+
+	const articles = data.articles.map((article) => article.slug);
+
+	return await sitemap.response({
+		origin: 'https://ankddev.vercel.app',
+		paramValues: {
+			'/articles/[...slug]': articles
+		},
+		defaultChangefreq: 'daily',
+		defaultPriority: 0.7
+	});
+};
