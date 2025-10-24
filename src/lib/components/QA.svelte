50a2cb8e8d6583329e3dabf28b16d71b1fd8595c
Andrey <190183925+ankddev@users.noreply.github.com>
2025-10-24 17:44:48 +0300

    refactor: improve layout for QA

--- src/lib/components/QA.svelte
+++ src/lib/components/QA.svelte
@@ -6,8 +6,8 @@
 
 <div class="w-full block">
 	<div class="font-bold">Q: {question}</div>
-	<div class="text-driftwood-900">
-		<span class="font-bold text-brand-foreground">A:</span>
+	<span class="font-bold">A:</span>
+	<span class="text-driftwood-900">
 		{answer}
-	</div>
+	</span>
 </div>
