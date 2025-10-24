5299046d0dab4698ae9d2038db54648594582aef
Andrey <190183925+ankddev@users.noreply.github.com>
2025-10-24 14:25:36 +0300

    feat: fetch filtered posts even if query less than 3 chars

--- src/routes/(standard)/articles/+page.server.ts
+++ src/routes/(standard)/articles/+page.server.ts
@@ -4,7 +4,7 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = async ({ fetch, url }) => {
 	const query = url.searchParams.get('q') || '';
 	const apiUrl =
-		query && query.length >= 3 ? `/api/articles?q=${encodeURIComponent(query)}` : '/api/articles';
+		query && query.length ? `/api/articles?q=${encodeURIComponent(query)}` : '/api/articles';
 	const response = await fetch(apiUrl);
 	const data: ArticlesResponse = await response.json();
 
